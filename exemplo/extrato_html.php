b12330c472c66c2ca7de78c0bf3e1aee004f9b54
Fernando Melo <melo.tads@gmail.com>
2023-10-26 16:48:58 -0300

    feat: atualiza versão do php, e das libs

---
+++ exemplo/extrato_html.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use ArleyOliveira\CFe\Extrato;
+
+$xml = __DIR__ . '/cfe.xml';
+
+$logo = __DIR__ . '/logo.jpg';
+
+$infoConsultaAplicativo = "Consulte o QR Code pelo aplicativo \"De olho na nota\", disponível na AppStore(Apple) e PlayStore(Android)";
+
+$extrato = new Extrato($xml, $logo, $infoConsultaAplicativo);
+
+$html = $extrato->html();
+
+echo $html;
+
